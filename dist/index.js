31c7257977138a1443f4050efccf357a13afddbe
wildwinds86 <dclaydon@gmail.com>
2025-08-05 13:02:15 +0100

    second commit

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,11 @@
 import express from "express";
 const app = express();
 const PORT = 8080;
-app.use(express.static("."));
+app.use("/app", express.static("./src/app"));
+app.get("/healthz", (req, res) => {
+    res.set({ "Content-Type": "text/plain; charset=utf-8" });
+    res.send("OK");
+});
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,12 @@ import path from "path";
 const app = express();
 const PORT = 8080;
 
-app.use(express.static("."));
+app.use("/app", express.static("./src/app"));
+
+app.get("/healthz", (req, res) => {
+  res.set({"Content-Type": "text/plain; charset=utf-8"});
+  res.send("OK");
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
