9a6ec3eb2e92a3a2f3e8200fce6799a9c612eeeb
wildwinds86 <dclaydon@gmail.com>
2025-08-05 14:52:42 +0100

    finished middleware lesson

--- dist/index.js
+++ dist/index.js
@@ -2,6 +2,7 @@ import express from "express";
 const app = express();
 const PORT = 8080;
 app.use("/app", express.static("./src/app"));
+app.use(middlewareLogResponses);
 app.get("/healthz", (req, res) => {
     res.set({ "Content-Type": "text/plain; charset=utf-8" });
     res.send("OK");
@@ -9,3 +10,12 @@ app.get("/healthz", (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
+function middlewareLogResponses(req, res, next) {
+    res.on("finish", () => {
+        const status = res.statusCode;
+        if (status < 200 || status >= 300) {
+            console.log(`[NON-OK] ${req.method} ${req.url} - Status: ${status}`);
+        }
+    });
+    next();
+}
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,26 @@
 import express from "express";
+import { Request, Response, NextFunction } from 'express';
 import path from "path";
+
+function middlewareLogResponses(req: Request, res: Response, next: NextFunction) {
+  (res as any).on("finish", () => {
+    const status = (res as any).statusCode;
+    if (status < 200 || status >= 300) {
+      console.log(`[NON-OK] ${req.method} ${req.url} - Status: ${status}`)
+    }
+
+  });
+  next();
+}
 
 const app = express();
 const PORT = 8080;
 
 app.use("/app", express.static("./src/app"));
+app.use(middlewareLogResponses);
 
 app.get("/healthz", (req, res) => {
-  res.set({"Content-Type": "text/plain; charset=utf-8"});
+  res.set({ "Content-Type": "text/plain; charset=utf-8" });
   res.send("OK");
 })
 
