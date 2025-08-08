ab4ed2fdfabe0d5e5868dcce66161f94a6d6d236
David <dclaydon@gmail.com>
2025-08-08 13:17:47 +0100

    Ch6L3

--- dist/config.js
+++ dist/config.js
@@ -1,5 +1,7 @@
+process.loadEnvFile();
 export const config = {
     fileServerHits: 0,
+    dbURL: String(process.env.DB_URL),
 };
 export const errorMsgs = {
     genericErrorMessage: "Something went wrong on our end",
